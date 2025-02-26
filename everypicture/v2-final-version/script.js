21a5f0663e50a84d33eadd97ae7f780e3b158f42
Kylie <kyliewong@Kylies-MacBook-Pro-2.local>
2025-02-25 23:51:19 -0800

    v2picture

---
+++ everypicture/v2-final-version/script.js
@@ -0,0 +1,20 @@
+(function() {
+    'use strict';
+    console.log('reading js');
+    const popup = document.querySelector('#popup');
+    const popupImg = document.querySelector('#popup-img');
+    const closePopup = document.querySelector('.close');
+    
+    // when first img is clicked inside a pair, make popup the alt img in the pair
+    document.querySelectorAll('.img-pair').forEach(pair => {
+        pair.addEventListener('click', function() {
+            const altImg = this.querySelector('.alt'); // change to alt img
+            popupImg.src = altImg.src; // make popup the alt img
+            popup.style.display = 'flex'; // change style of popup
+        });
+    });
+
+    closePopup.addEventListener('click', function() {
+        popup.style.display = 'none'; // closes popup
+    });
+})();
