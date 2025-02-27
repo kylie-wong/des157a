5967decda7365edc29f4d992004ed61faf2b358d
Kylie <kyliewong@campus-035-001.ucdavis.edu>
2025-02-27 13:58:25 -0800

    newv2

--- everypicture/v2-final-version/script.js
+++ everypicture/v2-final-version/script.js
@@ -6,15 +6,15 @@
     const closePopup = document.querySelector('.close');
     
     // when first img is clicked inside a pair, make popup the alt img in the pair
-    document.querySelectorAll('.img-pair').forEach(pair => {
+    document.querySelectorAll('.img-pair').forEach(function(pair) {
         pair.addEventListener('click', function() {
-            const altImg = this.querySelector('.alt'); // change to alt img
-            popupImg.src = altImg.src; // make popup the alt img
-            popup.style.display = 'flex'; // change style of popup
+        const altImg = this.querySelector('.alt'); // change to alt img
+        popupImg.src = altImg.src; // make popup the alt img
+        popup.style.display = 'flex'; // change style of popup
         });
     });
 
     closePopup.addEventListener('click', function() {
-        popup.style.display = 'none'; // closes popup
+        popup.style.display = 'none'; // closes popup on click
     });
 })();
